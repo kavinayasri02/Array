b410422a67d9466eb95da8125cb8f7c986edef67
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-03 12:05:18 +0530

    Update NoparaWithreturn.java

--- NoparaWithreturn.java
+++ NoparaWithreturn.java
@@ -2,10 +2,11 @@ package METHODTYPES;
 
 public class NoparaWithreturn {
     static int getLuckyNumber(){
-      return 2;
+      return 5;
     }
     public static void main(String[] args) {
         int num = getLuckyNumber();
         System.out.println("LuckyNumber is " + num);
     }
 }
+output : LuckyNumber is 5
