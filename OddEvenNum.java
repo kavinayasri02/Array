9ff578be31f068934b8f8c217b60772d1afcfb2b
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-06 22:40:23 +0530

    Add files via upload

---
+++ OddEvenNum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class OddEvenNum{
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        int n = kavi.nextInt();
+        if(n%2 == 0){
+            System.out.println(n +" is even");
+        }else{
+            System.out.println(n +" is odd");
+        }
+    }
+}
---
+++ OddEvenSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class OddEvenSum {
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        int a = kavi.nextInt();
+        int b = kavi.nextInt();
+        int sum = a + b;
+        if(sum%2 == 0){
+            System.out.println(sum +" is even");
+        }else{
+            System.out.println(sum +" is odd");
+        }
+    }
+}
