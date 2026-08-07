4aef6dcc83a3a2665f815f2339d56f93c8c7f2a9
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-08 00:18:00 +0530

    Add files via upload

---
+++ insertfirst.java
@@ -0,0 +1,14 @@
+public class insertfirst {
+    public static void main(String[]args){
+        int [] arr = { 10,20,30,40,50};
+        int newVal = 5;
+        int [] arr1 = new int[arr.length + 1];
+        arr1[0] = newVal;
+        for(int i =0;i<arr.length;i++){
+            arr1[i+1] = arr[i];
+        }
+        for(int i =0;i<arr1.length;i++ ){
+            System.out.print(arr1[i] + " ");
+        }
+    }
+}
