0d6b7532c1f62db507e242772aec0012b75aabad
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-10 15:12:12 +0530

    Add files via upload

---
+++ reverseofarr.java
@@ -0,0 +1,16 @@
+package ArrayProblems;
+import java.util.*;
+public class reverseofarr{
+    public static void main(String[] args) {
+        Scanner kavi = new Scanner(System.in);
+        int k = kavi.nextInt();
+        int[] arr = new int[k];
+        for(int i =0;i<k;i++){
+            arr[i] = kavi.nextInt();
+        }
+        for(int i = k-1;i>=0;i--){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
